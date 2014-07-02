b591c81906f29638c9f0d6bdc4cd7c9fd8b37867
onjiro <onjiro.mohyahya@gmail.com>
2014-07-02 14:47:01 +0900

    Update deploy task

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,10 +37,13 @@ set :rbenv_ruby, '2.0.0-p247'
 
 namespace :deploy do
 
-  desc 'Replace the database.yml'
-  task :replace_db_setting do
+  desc 'Replace env dependent files'
+  task :replace_settings do
     on roles(:app), in: :sequence do
       execute :cp, "#{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
+      execute :cp, "#{deploy_to}/shared/config/secrets.yml #{release_path}/config/secrets.yml"
+      execute :cp, "#{deploy_to}/shared/config/unicorn.rb #{release_path}/config/unicorn.rb"
+      execute :echo, "$PATH"
     end
   end
 
@@ -49,11 +52,13 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
+      execute :mkdir, "-p #{release_path}/tmp/pids"
       invoke 'unicorn:restart'
     end
   end
 
-  after :updating, :replace_db_setting
+  after :updating, :replace_settings
+  after :published, :restart
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
