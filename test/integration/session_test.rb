9954b716ceb8e15351110df7a7d095724b97fded
onjiro <onjiro.mohyahya@gmail.com>
2014-07-07 15:25:39 +0900

    Remove trial test

--- test/integration/session_test.rb
+++ test/integration/session_test.rb
@@ -2,10 +2,6 @@
 require 'test_helper'
 
 class SessionTest < ActionDispatch::IntegrationTest
-  test "the truth" do
-    assert { true }
-  end
-
   test 'show session:index on visit root' do
     visit '/'
     assert { current_path == '/' }
