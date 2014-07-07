72a3893f57068c480fbb83e24f3fb4aca2a1e1e5
onjiro <onjiro.mohyahya@gmail.com>
2014-07-07 15:23:16 +0900

    Add assertion for content of session:index

--- test/integration/session_test.rb
+++ test/integration/session_test.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'test_helper'
 
 class SessionTest < ActionDispatch::IntegrationTest
@@ -8,5 +9,6 @@ class SessionTest < ActionDispatch::IntegrationTest
   test 'show session:index on visit root' do
     visit '/'
     assert { current_path == '/' }
+    assert { page.has_content? 'Todo さん' }
   end
 end
