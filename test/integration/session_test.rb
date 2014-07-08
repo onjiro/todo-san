8a13bec33bbbfc951b4012ab2b76e6293c093eeb
onjiro <onjiro.mohyahya@gmail.com>
2014-07-08 12:08:27 +0900

    Add test case for click login as developer button

--- test/integration/session_test.rb
+++ test/integration/session_test.rb
@@ -10,7 +10,22 @@ class SessionTest < ActionDispatch::IntegrationTest
 
   test 'session:index has login buttons' do
     visit '/'
-    assert { page.has_css? 'a.login-btn-dev' }
-    assert { page.has_css? 'a.login-btn-twitter' }
+    assert { page.has_css? '#login-btn-dev' }
+    assert { page.has_css? '#login-btn-twitter' }
+  end
+
+  test 'show login page on click login buttun' do
+    visit '/'
+    click_link 'login-btn-dev'
+    assert { current_path == '/auth/developer' }
+    assert { page.has_css? 'input#name' }
+    assert { page.has_css? 'input#email' }
+  end
+  test 'show login page on click login buttun' do
+    visit '/'
+    click_link 'login-btn-dev'
+    assert { current_path == '/auth/developer' }
+    assert { page.has_css? 'input#name' }
+    assert { page.has_css? 'input#email' }
   end
 end
