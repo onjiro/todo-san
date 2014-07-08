d664a78bc9ba1edf530fbe2470c33abf0862e748
onjiro <onjiro.mohyahya@gmail.com>
2014-07-08 12:17:27 +0900

    Add white space for more readable

--- test/integration/session_test.rb
+++ test/integration/session_test.rb
@@ -4,12 +4,14 @@ require 'test_helper'
 class SessionTest < ActionDispatch::IntegrationTest
   test 'show session:index on visit root' do
     visit '/'
+
     assert { current_path == '/' }
     assert { page.has_content? 'Todo さん' }
   end
 
   test 'session:index has login buttons' do
     visit '/'
+
     assert { page.has_css? '#login-btn-dev' }
     assert { page.has_css? '#login-btn-twitter' }
   end
@@ -17,6 +19,7 @@ class SessionTest < ActionDispatch::IntegrationTest
   test 'show login page on click login buttun' do
     visit '/'
     click_link 'login-btn-dev'
+
     assert { current_path == '/auth/developer' }
   end
 
@@ -25,6 +28,7 @@ class SessionTest < ActionDispatch::IntegrationTest
     fill_in 'name' , :with => 'User Name'
     fill_in 'email', :with => 'email@integration.test.com'
     click_button 'Sign In'
+
     assert { current_path == '/todos' }
   end
 end
