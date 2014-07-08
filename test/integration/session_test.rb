05ae63d1da5e86b5d3c82fbc246a71307c051842
onjiro <onjiro.mohyahya@gmail.com>
2014-07-08 12:35:49 +0900

    Refactor session_test.rb using `setup`

--- test/integration/session_test.rb
+++ test/integration/session_test.rb
@@ -2,29 +2,26 @@
 require 'test_helper'
 
 class SessionTest < ActionDispatch::IntegrationTest
-  test 'show session:index on visit root' do
-    visit '/'
+  setup { visit '/' }
 
+  test 'show session:index on visit root' do
     assert { current_path == '/' }
     assert { page.has_content? 'Todo さん' }
   end
 
   test 'session:index has login buttons' do
-    visit '/'
-
     assert { page.has_css? '#login-btn-dev' }
     assert { page.has_css? '#login-btn-twitter' }
   end
 
   test 'show login page on click login buttun' do
-    visit '/'
     click_link 'login-btn-dev'
 
     assert { current_path == '/auth/developer' }
   end
 
   test 'show todos:index on sign in' do
-    visit '/auth/developer'
+    click_link 'login-btn-dev'
     fill_in 'name' , :with => 'User Name'
     fill_in 'email', :with => 'email@integration.test.com'
     click_button 'Sign In'
