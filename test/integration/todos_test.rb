48b808d28d855d02b4bbaf6ed240e39b2649fc38
onjiro <onjiro.mohyahya@gmail.com>
2014-07-10 21:42:31 +0900

    Add a case for new todo

--- test/integration/todos_test.rb
+++ test/integration/todos_test.rb
@@ -2,9 +2,12 @@
 require 'test_helper'
 
 class TodosTest < ActionDispatch::IntegrationTest
-  setup { sign_in user: 'hoge', email: 'email@integration.test.com' }
-  teardown { sign_out }
-  teardown { DatabaseRewinder.clean }
+  setup do
+    sign_in user: 'todo test user', email: 'email@integration.test.com'
+  end
+  teardown do
+    sign_out
+  end
 
   test 'show todos:index after sign in' do
     assert { page.has_content? 'Todo さん' }
@@ -14,14 +17,25 @@ class TodosTest < ActionDispatch::IntegrationTest
   test 'show new todo entry form on finish loaded' do
     assert { page.has_content? 'ロード中...' }
 
-    assert { page.has_no_content? 'ロード中...' }
+    wait_for_load_finished
     assert { page.has_css? '.todo-entry-form' }
   end
 
   test 'show no todos for new user' do
-    assert { page.has_no_content? 'ロード中...' }
-
+    wait_for_load_finished
     assert { page.has_no_css? '.todo-item' }
   end
 
+  test 'show new todo on enter new todo' do
+    wait_for_load_finished
+    fill_in 'newtodo-content', with: 'hoge'
+    click_link 'add-new-todo-btn'
+
+    assert { page.has_css? '.todo-item' } # view の更新を待つ
+    assert { page.all('.todo-item').length == 1 }
+  end
+
+  def wait_for_load_finished()
+    assert { page.has_no_content? 'ロード中...' }
+  end
 end
